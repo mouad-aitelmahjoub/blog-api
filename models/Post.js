5ead6430cffe6ed4827d25cf86f84c7026a8e757
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-10-14 12:00:02 +0100

    Create models

---
+++ models/Post.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("Post", PostSchema)
