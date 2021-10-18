9fe468137fb9f2d7630abfbe8f677dd45c93c02c
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-10-18 10:12:06 +0100

    Registration

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
+app.use(express.json())
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err))
 
+app.use("/api/auth", authRoute)
+
 app.listen("5000", () => {
   console.log("Backend is running now ")
-})
-
-app.use("/", (req, res) => {
-  console.log("this is main url")
 })
