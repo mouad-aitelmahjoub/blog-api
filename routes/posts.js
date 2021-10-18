081ae07e1dfefe7fe6e00880c0b1b547d39b9ee3
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-10-18 16:45:36 +0100

    Post Update

--- routes/posts.js
+++ routes/posts.js
@@ -6,10 +6,28 @@ const Post = require("../models/Post")
 //Create new Post
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body)
-
   try {
     const savedPost = await newPost.save()
     res.status(200).json(savedPost)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+//Update post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updatedPost)
+      } catch (error) {
+        res.status(500).json(error)
+      }
+    } else {
+      res.status(401).json("You can only update your posts")
+    }
   } catch (error) {
     res.status(500).json(error)
   }
