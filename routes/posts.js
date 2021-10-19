e2302b7c3ba131ae195991190d4bed27ba9e1e91
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-10-19 10:22:51 +0100

    Delete Post

--- routes/posts.js
+++ routes/posts.js
@@ -33,4 +33,23 @@ router.put("/:id", async (req, res) => {
   }
 })
 
+//Delete Post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.username === req.body.username) {
+      try {
+        await post.delete()
+        res.status(200).json("Post successfully deleted")
+      } catch (error) {
+        res.status(500).json(error)
+      }
+    } else {
+      res.status(401).json("You can only delete your posts")
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
 module.exports = router
