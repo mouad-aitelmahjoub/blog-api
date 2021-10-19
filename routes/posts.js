26c8e677163a802f93c93df19aad6fb0c625b49a
mouad aem <mouad.aitelmahjoub@gmail.com>
2021-10-19 11:21:16 +0100

    Get One / All posts

--- routes/posts.js
+++ routes/posts.js
@@ -52,4 +52,37 @@ router.delete("/:id", async (req, res) => {
   }
 })
 
+//Get One Post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    res.status(200).json(post)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+//Get All Posts
+router.get("/", async (req, res) => {
+  const username = req.query.user
+  const category = req.query.cat
+  try {
+    let posts
+    if (username) {
+      posts = await Post.find({ username })
+    } else if (category) {
+      posts = await Post.find({
+        categories: {
+          $in: [category],
+        },
+      })
+    } else {
+      posts = await Post.find()
+    }
+    res.status(200).json(posts)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
 module.exports = router
